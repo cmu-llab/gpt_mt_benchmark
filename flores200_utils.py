6a3ee7704ba278f4da144c00fdbfebf4cf09cf0f
Perez Ogayo <perezogayo@gmail.com>
2023-06-05 02:50:47 +0300

    translated 1 line from the test set

---
+++ flores200_utils.py
@@ -0,0 +1,32 @@
+import os
+
+# entries = os.listdir('data/flores200_dataset/devtest')
+# for entry in entries:
+#     print(entry)
+
+def load_test_set(name, number=None):
+    f = open(name, 'r')
+    lines =[line.strip() for line in f.readlines()]
+    lang_code = name.split("/")[-1].split("_")[0]
+    result= {"lang_code": lang_code}
+    if number:
+        result ["lines"] = lines[:number]
+    else:
+        result["lines"] = lines
+    return result
+
+def load_all_tests(test_dir, number=None):
+    entries = os.listdir(test_dir)
+    test_sets = {}
+    for entry in entries:
+        f_name = os.path.join(test_dir, entry)
+        data = load_test_set(f_name, number)
+        test_sets[data["lang_code"]] = data["lines"]
+    return test_sets
+
+
+if __name__ == '__main__':
+    test_dir= 'data/flores200_dataset/devtest'
+    print("here")
+    test_data = load_all_tests(test_dir, 1)
+    print(test_data)
